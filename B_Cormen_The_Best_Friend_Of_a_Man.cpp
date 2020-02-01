7fec1a98f4e152a9a781ba77992f4cf445249000
Axel Prieto <prietoaxel@gmail.com>
2020-02-01 16:00:35 +0100

    minor changes

---
+++ B_Cormen_The_Best_Friend_Of_a_Man.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+// https://codeforces.com/problemset/problem/732/B
+int32_t main()
+{
+    int n, k;
+    cin >> n >> k;
+
+    int input;
+    vector<int> a;
+    while (cin >> input)
+        a.push_back(input);
+    
+
+
+    cout << 0;
+}
