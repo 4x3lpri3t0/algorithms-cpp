76a69dd2143df4fbcfc8635657e75ffa199f3884
Axel Prieto <prietoaxel@gmail.com>
2023-07-10 11:15:17 +0100

    Daily programming practice

---
+++ UVa/Volume 114/11498 - Division of Nlogonia/11498 - Division of Nlogonia.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int TC, N, M, x, y;
+    cin >> TC;
+    while (TC != 0)
+    {
+        cin >> N;
+        cin >> M;
+
+        while (TC-- > 0)
+        {
+            cin >> x;
+            cin >> y;
+
+            if (x == N || y == M)
+                cout << "divisa\n";
+            else if (x > N && y > M)
+                cout << "NE\n";
+            else if (x < N && y > M)
+                cout << "NO\n";
+            else if (x < N && y < M)
+                cout << "SO\n";
+            else
+                cout << "SE\n";
+        }
+
+        cin >> TC;
+    }
+}
