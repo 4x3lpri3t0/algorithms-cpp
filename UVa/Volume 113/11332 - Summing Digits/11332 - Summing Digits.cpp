a6bd5cd3bea0789d2548e094342cb3d2f16a81ed
Axel Prieto <prietoaxel@gmail.com>
2023-07-14 11:03:18 +0100

    Daily programming practice

---
+++ UVa/Volume 113/11332 - Summing Digits/11332 - Summing Digits.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    long long x;
+    int sum;
+
+    while (scanf("%lld", &x))
+    {
+        if (x == 0)
+            break;
+
+        while (x / 10 != 0)
+        {
+            sum = 0;
+            while (x != 0)
+            {
+                sum += x % 10;
+                x /= 10;
+            }
+            x = sum;
+        }
+        printf("%lld\n", x);
+    }
+}
