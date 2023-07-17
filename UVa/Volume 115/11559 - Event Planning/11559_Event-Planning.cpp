37806dc936540b3b8f9814ec888794b74ed26abb
Axel Prieto <prietoaxel@gmail.com>
2023-07-17 10:13:33 +0100

    Daily programming practice

--- UVa/Volume 115/11559 - Event Planning/11559_Event-Planning.cpp
+++ UVa/Volume 115/11559 - Event Planning/11559_Event-Planning.cpp
@@ -1,11 +1,30 @@
-#include <cstdio>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main()
 {
-    int n, k = 0;
-    scanf("%d", &n);
+    int N, B, H, W, hotelCostPerNight, bedsAvailable;
+    while (scanf("%d %d %d %d\n", &N, &B, &H, &W) != EOF)
+    {
+        int minCost = B + 1;
+        for (int i = 0; i < H; i++)
+        {
+            cin >> hotelCostPerNight;
 
-    // TODO
+            for (int j = 0; j < W; j++)
+            {
+                cin >> bedsAvailable;
+                if (bedsAvailable >= N && N * hotelCostPerNight < minCost)
+                {
+                    minCost = N * hotelCostPerNight;
+                }
+            }
+        }
+
+        if (minCost <= B)
+            printf("%d\n", minCost);
+        else
+            printf("%s\n", "stay home");
+    }
 }
