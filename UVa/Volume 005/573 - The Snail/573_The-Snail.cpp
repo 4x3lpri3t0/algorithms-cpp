1e0c0f6d6f5d8782ae5c45c742f848d2367c549d
Axel Prieto <prietoaxel@gmail.com>
2023-07-22 15:46:28 +0100

    Daily programming practice

---
+++ UVa/Volume 005/573 - The Snail/573_The-Snail.cpp
@@ -0,0 +1,17 @@
+// https://onlinejudge.org/external/5/573.pdf
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main()
+{
+    int H, U, D, F;
+    cin >> H >> U >> D >> F;
+
+    while (H == 0)
+    {
+        // TODO ...
+
+        cin >> H >> U >> D >> F;
+    }
+}
