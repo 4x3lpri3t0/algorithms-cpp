0639cc0b55b5ae6ffd54477f824c99bac9707116
Axel Prieto <prietoaxel@gmail.com>
2023-07-25 11:29:08 +0100

    Daily programming practice

---
+++ UVa/Volume 103/10324 - Zeros and Ones/10324_Zeros-and-Ones.cpp
@@ -0,0 +1,37 @@
+// https://onlinejudge.org/external/103/10324.pdf
+#include <cstdio>
+
+int main()
+{
+    char s[1000005];
+
+    int N, a, b, temp, round(1);
+    bool valid;
+
+    while (scanf("%s %d", s, &N) == 2)
+    {
+        printf("Case %d:\n", round++);
+
+        while (N--)
+        {
+            scanf("%d%d", &a, &b);
+            if (b < a)
+            {
+                temp = a;
+                a = b;
+                b = temp;
+            }
+
+            valid = true;
+
+            for (int i = a; i < b; ++i)
+                if (s[i] != s[i + 1])
+                    valid = false;
+
+            if (valid)
+                printf("Yes\n");
+            else
+                printf("No\n");
+        }
+    }
+}
