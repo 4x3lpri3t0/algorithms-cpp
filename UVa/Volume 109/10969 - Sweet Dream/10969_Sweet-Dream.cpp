c987d744d2651184a3bb175b777fa5ff91cea83f
Axel Prieto <prietoaxel@gmail.com>
2023-07-27 10:56:14 +0100

    Daily programming practice

---
+++ UVa/Volume 109/10969 - Sweet Dream/10969_Sweet-Dream.cpp
@@ -0,0 +1,15 @@
+// https://onlinejudge.org/external/109/10919.pdf
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main()
+{
+    int k, m;
+    while (cin >> k, k > 0)
+    {
+        cin >> m;
+
+        // TODO ...
+    }
+}
