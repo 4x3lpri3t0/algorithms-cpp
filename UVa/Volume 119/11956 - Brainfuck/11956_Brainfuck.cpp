d60007b891d9ed5d84ea6818a59e32c02c824e17
Axel Prieto <prietoaxel@gmail.com>
2023-08-09 10:52:08 +0100

    Daily programming practice

---
+++ UVa/Volume 119/11956 - Brainfuck/11956_Brainfuck.cpp
@@ -0,0 +1,15 @@
+// https://onlinejudge.org/external/119/11956.pdf
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main()
+{
+    int T;
+    cin >> T;
+    
+    while (T--)
+    {
+        // TODO
+    }
+}
