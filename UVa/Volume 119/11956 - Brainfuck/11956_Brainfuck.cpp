1dc1f4c06c065a7766e7f81e50749dbaca801292
Axel Prieto <prietoaxel@gmail.com>
2023-08-14 11:23:03 +0100

    Daily programming practice

--- UVa/Volume 119/11956 - Brainfuck/11956_Brainfuck.cpp
+++ UVa/Volume 119/11956 - Brainfuck/11956_Brainfuck.cpp
@@ -1,15 +1,56 @@
 // https://onlinejudge.org/external/119/11956.pdf
-#include <bits/stdc++.h>
-#define int long long
-using namespace std;
+#include <cstdio>
 
-int32_t main()
+int main()
 {
-    int T;
-    cin >> T;
-    
-    while (T--)
+    char input[100001];
+
+    int mem[100];
+    int T, current;
+
+    scanf("%d", &T);
+
+    for (int t = 1; t <= T; ++t)
     {
-        // TODO
+        for (int i = 0; i < 100; ++i)
+            mem[i] = 0;
+
+        current = 0;
+
+        scanf("%s", &input);
+
+        for (int i = 0; input[i]; ++i)
+        {
+            switch (input[i])
+            {
+            case '>':
+                ++current;
+                if (current == 100)
+                    current = 0;
+                break;
+
+            case '<':
+                --current;
+                if (current == -1)
+                    current = 99;
+                break;
+
+            case '+':
+                ++mem[current];
+                if (mem[current] == 256)
+                    mem[current] = 0;
+                break;
+
+            case '-':
+                --mem[current];
+                if (mem[current] == -1)
+                    mem[current] = 255;
+            }
+        }
+        
+        printf("Case %d:", t);
+        for (int i = 0; i < 100; ++i)
+            printf(" %02X", mem[i]);
+        printf("\n");
     }
 }
