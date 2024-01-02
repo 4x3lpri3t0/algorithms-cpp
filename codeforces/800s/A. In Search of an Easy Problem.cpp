eee26e939441868dcd3f489db9a0a418e460ac67
Axel Prieto <prietoaxel@gmail.com>
2024-01-02 15:48:59 -0300

    Daily programming practice

---
+++ codeforces/800s/A. In Search of an Easy Problem.cpp
@@ -0,0 +1,26 @@
+// https://codeforces.com/problemset/problem/1030/A
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    int n;
+    cin >> n;
+
+    // Read int array:
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    
+    // Check if there is a 1:
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == 1)
+        {
+            cout << "HARD" << endl;
+        }
+    }
+    
+    cout << "EASY" << endl;
+}
---
+++ codeforces/800s/B. Drinks.cpp
@@ -0,0 +1,26 @@
+// https://codeforces.com/problemset/problem/200/B
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    int n;
+    cin >> n;
+
+    // Read int array:
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+        
+    // Calculate sum:
+    double sum = 0;
+    for (int i = 0; i < n; i++)
+        sum += arr[i];
+
+    // Calculate average:
+    double avg = sum / n;
+
+    // Print result:
+    cout << avg << endl;
+}
